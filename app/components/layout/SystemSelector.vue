980879a074220e764e1348830d246835c3f4c5d8
Rubén Conde Magaña <code@rubn.dev>
2026-01-12 10:37:28 +0100

    ✨ Enhance Imperial System Calculator with dynamic unit conversion and pricing functionality

--- app/components/layout/SystemSelector.vue
+++ app/components/layout/SystemSelector.vue
@@ -1,7 +1,7 @@
 <template>
-    <UContainer class="fixed bottom-0 p-20 min-w-screen rounded-none flex flex-row justify-center items-center gap-2 mt-5 text-sm">
+    <UContainer class="fixed bottom-0 pb-20 min-w-screen rounded-none flex flex-row justify-center items-center gap-2 text-sm">
         <span>📐 Metric System</span>
-        <USwitch v-model="useImperialSystem" size="xl" color="neutral" />
+        <USwitch v-model="state.useImperialSystem" size="xl" color="neutral" />
         <span> "Freedom" Units 🦅</span>
     </UContainer>
 </template>
@@ -10,11 +10,6 @@
 import { useGlobalState } from '~/store';
 
 const state = useGlobalState()
-const useImperialSystem = ref(state.value.useImperialSystem);
-
-watch(useImperialSystem, (newValue) => {
-    state.value.useImperialSystem = newValue;
-});
 </script>
 
 <style scoped></style>
