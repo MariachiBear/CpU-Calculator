7a024abfdf0f90fac5d55b53c762115155778616
Rubén Conde Magaña <code@rubn.dev>
2026-01-12 06:48:53 +0100

    🔧 Update nuxt.config.ts for improved UI configuration and formatting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
+  ui: {
+    experimental: {
+      componentDetection: true,
+    },
+  },
+  css: ["~/assets/css/main.css"],
+
   modules: [
-    '@nuxt/a11y',
-    '@nuxt/eslint',
-    '@nuxt/hints',
-    '@nuxt/test-utils',
-    '@nuxt/ui',
-    '@vite-pwa/nuxt'
-  ]
-})
+    "@nuxt/a11y",
+    "@nuxt/eslint",
+    "@nuxt/hints",
+    "@nuxt/test-utils",
+    "@nuxt/ui",
+    "@vite-pwa/nuxt",
+  ],
+});
